30c9865e68687a99a059191a88f059252b70046b
Martins Ierags <ieris@inbox.lv>
2025-01-19 17:04:36 +0000

    Fix for install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # Create virtual environment if it doesn't exist
 if [ ! -d ".venv" ]; then
     echo "Creating virtual environment..."
-    python -m venv .venv
+    python3 -m venv .venv
 fi
 
 # Activate virtual environment
